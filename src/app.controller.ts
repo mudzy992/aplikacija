e7b54434e63c33aed3d16323f1d7b1b21df422cb
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2021-11-22 21:34:33 +0100

    NestJS Aplikacija
    Start

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get() // ovako izgleda homepage locahost:3000
+  getHello(): string {
+    return "hello world home page";
+  }
+  @Get("world") // dodajemo link localhost:3000/world
+  getWorld(): string {
+    return "hello world";
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core'; //iz cijelog ovog paketa mi importujemo klasu NestFactory
+import { AppModule } from './app.module'; 
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
