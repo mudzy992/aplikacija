61e22177116f517b1e2114875bda7459bc117195
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2021-11-27 15:34:41 +0100

    Izmjene controllera api.administrator
    Urađene izmjene za ADD i EDIT admin, testirano putem postmana.
    Dodane funkcije grešaka.

--- src/controllers/api/administrator.controller.ts
+++ src/controllers/api/administrator.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { Administrator } from 'entities/administrator.entity';
 import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { AdministratorService } from 'src/services/administrator/administrator.service';
 
 @Controller('api/administrator') //kada ovdje dodamo prefiks, onda ne moramo dolje ispod, podrazumijeva se
@@ -21,9 +22,17 @@ export class AdministratorController {
   // tu anotaciju vršimo kao ispod getById(anotacija)
   // Param je specijalna anotacija koja kaže: Ovaj parametar (u zagradi)
   // uzmi iz linka (iznad), i stavi u konstantu administratorId: number
-  getById(@Param('id') administratorId: number): Promise<Administrator> {
+  getById(
+    @Param('id') administratorId: number,
+  ): Promise<Administrator | ApiResponse> {
     // Vraća isto što i servis, obećanje da će vratiti jednog administratora
-    return this.administratorService.getById(administratorId);
+    return new Promise(async (resolve) => {
+      const admin = await this.administratorService.getById(administratorId);
+      if (admin === undefined) {
+        resolve(new ApiResponse('error', -1002));
+      }
+      resolve(admin);
+    });
   }
   // Dodavanje novog administratora
   // prvo što treba jeste da postoji ruta preko koje će se to izvršavati
@@ -35,7 +44,7 @@ export class AdministratorController {
   // Otvori taj neki link localhost:3003/api/administrator/
   // i uzmi iz tijela tog linka data
   // u formatu koji je definisan u Dto (AddAdministratorDto)
-  add(@Body() data: AddAdministratorDto): Promise<Administrator> {
+  add(@Body() data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
     return this.administratorService.add(data);
   }
 
@@ -43,7 +52,7 @@ export class AdministratorController {
   edit(
     @Param('id') id: number,
     @Body() data: EditAdministratorDto,
-  ): Promise<Administrator> {
+  ): Promise<Administrator | ApiResponse> {
     return this.administratorService.editById(id, data);
   }
 }
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
+import { resolve } from 'path/posix';
 import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -21,7 +23,7 @@ export class AdministratorService {
     return this.administrator.findOne(id);
   }
 
-  add(data: AddAdministratorDto): Promise<Administrator> {
+  add(data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
     const crypto = require('crypto');
     const passwordHash = crypto.createHash('sha512');
     passwordHash.update(data.password);
@@ -29,22 +31,34 @@ export class AdministratorService {
 
     const newAdmin: Administrator = new Administrator();
     newAdmin.username = data.username;
-    newAdmin.passwordHash = data.password;
+    newAdmin.passwordHash = passwordHashString;
 
-    return this.administrator.save(newAdmin);
+    return new Promise((resolve) => {
+      this.administrator
+        .save(newAdmin)
+        .then((data) => resolve(data))
+        .catch((error) => {
+          const response: ApiResponse = new ApiResponse('error', -1001);
+          resolve(response);
+        });
+    });
   }
 
   async editById(
     id: number,
     data: EditAdministratorDto,
-  ): Promise<Administrator> {
+  ): Promise<Administrator | ApiResponse> {
     const admin: Administrator = await this.administrator.findOne(id);
+    if (admin === undefined) {
+      return new Promise((resolve) => {
+        resolve(new ApiResponse('error', -1002));
+      });
+    }
     const crypto = require('crypto');
     const passwordHash = crypto.createHash('sha512');
     passwordHash.update(data.password);
     const passwordHashString = passwordHash.digest('hex').toUpperCase();
     admin.passwordHash = passwordHashString;
-
     return this.administrator.save(admin);
   }
 }
