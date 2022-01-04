e4b4025dd9793a911ed11c7cd5e35fc3fd37dd5c
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-04 13:27:24 +0100

    Implamentacija UPLOAD mehanizma
    Upload mehanizam slika dodat na article servis

--- src/services/photo/photo.service.ts
+++ src/services/photo/photo.service.ts
@@ -12,4 +12,7 @@ export class PhotoService extends TypeOrmCrudService<Photo> {
   ) {
     super(photo);
   }
+  add(newPhoto: Photo): Promise <Photo>{
+    return this.photo.save(newPhoto);
+  }
 }
