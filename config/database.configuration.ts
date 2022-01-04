7d023aaa09192e70aa56dcd22bef7b286898d83e
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-04 21:11:24 +0100

    Update database.configuration.ts

--- config/database.configuration.ts
+++ config/database.configuration.ts
@@ -1,9 +1,9 @@
- export const DatabaseConfiguration = {
-   hostname: 'localhost',
-   username: 'mudzy',
-   password: 'Nije,kikiriki1',
-   database: 'aplikacija',
- };
+export const DatabaseConfiguration = {
+  hostname: 'localhost',
+  username: 'mudzy',
+  password: 'Nije,kikiriki1',
+  database: 'aplikacija',
+};
 
 // ovo je konekcija baze na poslu
 //export const DatabaseConfiguration = {
