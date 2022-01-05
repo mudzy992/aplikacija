47110b5bd021647601fe1e6bec320e21a541422f
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-05 20:56:29 +0100

    prettier

--- src/dtos/administrator/login.info.administrator.dto.ts
+++ src/dtos/administrator/login.info.administrator.dto.ts
@@ -1,11 +1,11 @@
 export class LoginInfoAdministratorDto {
-    administratorid: number;
-    username: string;
-    token: string;
-    
-    constructor(id: number, un: string, jwt: string) {
-        this.administratorid = id;
-        this.username = un;
-        this.token = jwt;
-    }
+  administratorid: number;
+  username: string;
+  token: string;
+
+  constructor(id: number, un: string, jwt: string) {
+    this.administratorid = id;
+    this.username = un;
+    this.token = jwt;
+  }
 }
