71a4cbf83b830d97389b86c12742f595351d7187
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-11 11:02:15 +0100

    Implementacija registracije korisnika

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -5,7 +5,7 @@
 //    određeni zahtjev upućen nama (username, password),
 //    odgovaraju nekom postojećem korisniku u bazi podataka
 
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Put, Req } from '@nestjs/common';
 import { LoginAdministratorDto } from 'src/dtos/administrator/login.administrator.dto';
 import { ApiResponse } from 'src/misc/api.response.class';
 import { AdministratorService } from 'src/services/administrator/administrator.service';
@@ -15,10 +15,12 @@ import * as jwt from 'jsonwebtoken';
 import { JWTDataAdministratorDto } from 'src/dtos/administrator/jwt.data.administrator.dto';
 import { Request } from 'express';
 import { jwtSecret } from 'config/jwt.secret';
+import { UserRegistrationDto } from 'src/dtos/user/user.registration.dto';
+import { UserService } from 'src/services/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(public administratorService: AdministratorService) {}
+  constructor(public administratorService: AdministratorService, public userService: UserService) {}
   // Obzirom da se radi o Post metodu koji zahtjeva tijelo objekta,
   // potrebno je kreirati DTO (data transfer objekat)
   @Post('login')
@@ -94,5 +96,10 @@ export class AuthController {
       token,
     );
     return new Promise((resolve) => resolve(responseObject));
+  }
+  // REGISTRACIJA NOVOG KORISNIKA
+  @Put('user/register')
+  async userRegister(@Body() data: UserRegistrationDto) {
+    return await this.userService.register(data);
   }
 }
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { UserRegistrationDto } from 'src/dtos/user/user.registration.dto';
 import { User } from 'src/entities/user.entity';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<User> {
@@ -11,5 +14,33 @@ export class UserService extends TypeOrmCrudService<User> {
     private readonly user: Repository<User>, //Čim spomenenom neki repozitorijum moramo da taj repozitoriju evidentiramo u našem osnovnom modulu (app.module.ts)
   ) {
     super(user);
+  }
+  // METOD REGISTRACIJE (nije isto kao dodavanje POST)
+  async register(data: UserRegistrationDto): Promise<User | ApiResponse> {
+    const passwordHash = crypto.createHash('sha512');
+    passwordHash.update(data.password);
+    const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+    const newUser: User = new User();
+    newUser.email = data.email;
+    newUser.passwordHash = passwordHashString;
+    newUser.forename = data.forename;
+    newUser.surename = data.surename;
+    newUser.phoneNumber = data.phoneNumber;
+    newUser.postalAddress = data.postalAddress;
+
+    try {
+      const savedUser = await this.user.save(newUser);
+      if (!savedUser) {
+        throw new Error('');
+      }
+      return savedUser;
+    } catch (e) {
+      return new ApiResponse(
+        'error',
+        -6001,
+        'This user account cannot be created.',
+      );
+    }
   }
 }
