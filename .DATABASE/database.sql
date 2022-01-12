f12f15c55f1642bc7e022dd46f5b12e67bde25b3
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-12 12:40:30 +0100

    eh sad se igramo

---
+++ .DATABASE/database.sql
@@ -0,0 +1,232 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP DATABASE IF EXISTS `aplikacija`;
+CREATE DATABASE IF NOT EXISTS `aplikacija` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aplikacija`;
+
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE IF NOT EXISTS `administrator` (
+  `administrator_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `password_hash` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`administrator_id`),
+  UNIQUE KEY `uq_administrator_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `administrator`;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` (`administrator_id`, `username`, `password_hash`) VALUES
+	(1, 'Administrator', '887375DAEC62A9F02D32A63C9E14C7641A9A8A42E4FA8F6590EB928D9744B57BB5057A1D227E4D40EF911AC030590BBCE2BFDB78103FF0B79094CEE8425601F5');
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS `article` (
+  `article_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `excerpt` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('available','visible','hidden') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'available',
+  `is_promoted` tinyint unsigned NOT NULL DEFAULT '0',
+  `category_id` int unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`article_id`),
+  KEY `fk_article_category_id` (`category_id`),
+  CONSTRAINT `fk_article_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `article`;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` (`article_id`, `name`, `excerpt`, `description`, `status`, `is_promoted`, `category_id`, `created_at`) VALUES
+	(1, 'ACME SSD HD11 1TB', 'Kratki opis', 'Detaljan opis', 'visible', 1, 5, '2021-11-29 14:11:34'),
+	(2, 'SSD 500 GB', 'Kratki opis...', 'Detaljan opis...', 'available', 0, 5, '2021-11-29 14:11:50');
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `article_feature`;
+CREATE TABLE IF NOT EXISTS `article_feature` (
+  `article_feature_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int unsigned NOT NULL DEFAULT '0',
+  `feature_id` int unsigned NOT NULL DEFAULT '0',
+  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`article_feature_id`),
+  UNIQUE KEY `uq_article_feature_article_id_feature_id` (`article_id`,`feature_id`),
+  KEY `fk_article_feature_feature_id` (`feature_id`),
+  CONSTRAINT `fk_article_feature_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_article_feature_feature_id` FOREIGN KEY (`feature_id`) REFERENCES `feature` (`feature_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `article_feature`;
+/*!40000 ALTER TABLE `article_feature` DISABLE KEYS */;
+INSERT INTO `article_feature` (`article_feature_id`, `article_id`, `feature_id`, `value`) VALUES
+	(5, 2, 1, '500'),
+	(6, 2, 6, '2,5"'),
+	(7, 2, 2, 'Laptop'),
+	(8, 1, 1, '1024MB'),
+	(9, 1, 2, 'Laptop');
+/*!40000 ALTER TABLE `article_feature` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `article_price`;
+CREATE TABLE IF NOT EXISTS `article_price` (
+  `article_price_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int unsigned NOT NULL DEFAULT '0',
+  `price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`article_price_id`),
+  KEY `fk_article_price_article_id` (`article_id`),
+  CONSTRAINT `fk_article_price_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `article_price`;
+/*!40000 ALTER TABLE `article_price` DISABLE KEYS */;
+INSERT INTO `article_price` (`article_price_id`, `article_id`, `price`, `created_at`) VALUES
+	(1, 1, 75.00, '2021-11-29 14:13:38'),
+	(2, 2, 120.00, '2021-11-29 14:13:46'),
+	(3, 1, 59.77, '2022-01-11 07:44:26');
+/*!40000 ALTER TABLE `article_price` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `cart_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cart_id`),
+  KEY `fk_cart_user_id` (`user_id`),
+  CONSTRAINT `fk_cart_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `cart`;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` (`cart_id`, `user_id`, `created_at`) VALUES
+	(1, 1, '2021-11-29 14:41:32');
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `cart_article`;
+CREATE TABLE IF NOT EXISTS `cart_article` (
+  `cart_article_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `cart_id` int unsigned NOT NULL DEFAULT '0',
+  `article_id` int unsigned NOT NULL DEFAULT '0',
+  `quantity` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`cart_article_id`),
+  UNIQUE KEY `uq_cart_article_cart_id_article_id` (`cart_id`,`article_id`),
+  KEY `fk_cart_article_article_id` (`article_id`),
+  CONSTRAINT `fk_cart_article_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_cart_article_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `cart_article`;
+/*!40000 ALTER TABLE `cart_article` DISABLE KEYS */;
+INSERT INTO `cart_article` (`cart_article_id`, `cart_id`, `article_id`, `quantity`) VALUES
+	(1, 1, 1, 2);
+/*!40000 ALTER TABLE `cart_article` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `image_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `parent__category_id` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uq_category_name` (`name`),
+  UNIQUE KEY `uq_category_image_path` (`image_path`),
+  KEY `fk_category_parent__category_id` (`parent__category_id`),
+  CONSTRAINT `fk_category_parent__category_id` FOREIGN KEY (`parent__category_id`) REFERENCES `category` (`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `category`;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`category_id`, `name`, `image_path`, `parent__category_id`) VALUES
+	(1, 'Računarske komponente', 'assets/pc.jpg', NULL),
+	(2, 'Kućna elektronika', 'assets/home.jpg', NULL),
+	(3, 'Laptop računar', 'assets/pc/laptop.jpg', 1),
+	(4, 'Memorijski mediji', 'assets/pc/memory.jpg', 1),
+	(5, 'Hard diskovi', 'assets/pc/memory/hdd.jpg', 4);
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `feature`;
+CREATE TABLE IF NOT EXISTS `feature` (
+  `feature_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `category_id` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`feature_id`),
+  UNIQUE KEY `uq_feature_name_category_id` (`name`,`category_id`),
+  KEY `fk_feature_category_id` (`category_id`),
+  CONSTRAINT `fk_feature_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `feature`;
+/*!40000 ALTER TABLE `feature` DISABLE KEYS */;
+INSERT INTO `feature` (`feature_id`, `name`, `category_id`) VALUES
+	(5, 'Brzina (RPM)', 5),
+	(4, 'Cache (MB)', 5),
+	(1, 'Kapacitet (GB)', 5),
+	(6, 'Priključak', 5),
+	(12, 'Proizvođač', 2),
+	(11, 'Radni napon', 2),
+	(3, 'Veličina(inch)', 5),
+	(2, 'Vrsta (za)', 5);
+/*!40000 ALTER TABLE `feature` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cart_id` int unsigned NOT NULL DEFAULT '0',
+  `status` enum('rejected','accepted','shipped','pending') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`order_id`),
+  UNIQUE KEY `uq_order_cart_id` (`cart_id`),
+  CONSTRAINT `fk_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `order`;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` (`order_id`, `created_at`, `cart_id`, `status`) VALUES
+	(1, '2021-11-29 14:48:45', 1, 'pending');
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE IF NOT EXISTS `photo` (
+  `photo_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int unsigned NOT NULL DEFAULT '0',
+  `image_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`photo_id`),
+  UNIQUE KEY `uq_photo_image_path` (`image_path`),
+  KEY `fk_photo_article_id` (`article_id`),
+  CONSTRAINT `fk_photo_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `photo`;
+/*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+INSERT INTO `photo` (`photo_id`, `article_id`, `image_path`) VALUES
+	(12, 1, '2022112-3280913783-wd-blue-ssd-500gb.jpg');
+/*!40000 ALTER TABLE `photo` ENABLE KEYS */;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `password_hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `password_reset_link` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `forename` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `surename` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `phone_number` varchar(24) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `postal_address` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uq_user_email` (`email`),
+  UNIQUE KEY `uq_user_phone_number` (`phone_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`user_id`, `email`, `password_hash`, `password_reset_link`, `forename`, `surename`, `phone_number`, `postal_address`) VALUES
+	(1, 'mudi123@gmail.com', 'nije,kikirki', '0', 'Mudzahid', 'Ceric', '062429038', '71300'),
+	(2, 'm.ceric@epbih.ba', 'A5DFA812369F37FFD1755CA396C471CCEBC16B19DAEC09A1442287BCE01BD2BDC7603A2B86DF587FDAFA3EDEF4DA3E9D76E8B8194D24E696DCCA329CDD1429DE', '0', 'Mudzahid', 'Ceric', '062222222', '71300');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
